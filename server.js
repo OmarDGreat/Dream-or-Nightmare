bd5c76d70b739fa144cbad40b3297fa01fd2c6f7
OmarD <omar.daghestani@colorado.edu>
2022-09-15 17:45:23 -0600

    added to server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
