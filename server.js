fc3591b248b03302d6fe421e15984de67fc9cd91
OmarD <omar.daghestani@colorado.edu>
2022-09-15 18:27:56 -0600

    added user and post routes in controller/api

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controller');
 const sequelize = require('./config/connection');
 
 const app = express();
