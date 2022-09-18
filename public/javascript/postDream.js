4795b7549429f8615fd50d947f8a897bc0836f74
night <yseddiq@gmail.com>
2022-09-18 18:56:32 -0400

    added POST fetch request

--- public/javascript/postDream.js
+++ public/javascript/postDream.js
@@ -3,6 +3,17 @@ async function newDreamHandler(event){
     
     const title = document.querySelector('input[name="dream-title"]').value;
     const dreamPost=document.querySelector('textarea[name="dream-story"]').value;
+
+    const response = await fetch('api/dream_story',{
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            dreamPost
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 documnet.querySelector('#post-dream-form').addEventListener('submit', newDreamHandler);
