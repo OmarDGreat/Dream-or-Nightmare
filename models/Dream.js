4995653ebb991f21495770d2d97078cba2474dc1
night <yseddiq@gmail.com>
2022-09-18 19:40:11 -0400

    update dream model

--- models/Dream.js
+++ models/Dream.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-const { Model, DataTypes}=require('sequelize');
-const sequelize = require('../config/connection');
-
-//inherits peroperties from the Model class.
-class Dream extends Model{};
-
-//create columns for Dream model
-Dream.init(
-    {
-        dream_id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        dream_story:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
-                len: [10]
-            }
-        },
-        post_url:{
-            type: DataTypes.STRING,
-            allowNull:false,
-            validate:{
-                isURL: true,
-            }
-        },
-        upvote:{
-            type: DataTypes.INTEGER
-        },
-        downvote:{
-            type: DataTypes.INTEGER
-        }
-    },
-    {
-        sequelize,
-        freezeTableNames: true,
-        underscored: true,
-        modelName: 'dream'
-    }
-);
-
-module.exports = Dream;
-=======
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -68,13 +17,13 @@ Dream.init(
         type: DataTypes.STRING,
         allowNull: false
       },
-      Dream_url: {
+      dream_story:{
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          isURL: true
+        validate:{
+            len: [10]
         }
-      },
+    },
       user_id: {
         type: DataTypes.INTEGER,
         references: {
@@ -93,4 +42,3 @@ Dream.init(
 
 
   module.exports = Dream;
->>>>>>> c7147959f7352d3afbda1e4c295027715626ce69
