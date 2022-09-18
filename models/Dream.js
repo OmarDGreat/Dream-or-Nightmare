40b154cfa65d48efe20b59ea5cbba59d37b94e80
OmarD <omar.daghestani@colorado.edu>
2022-09-18 17:39:23 -0600

    resolving merge conflict

--- models/Dream.js
+++ models/Dream.js
@@ -1,96 +1,49 @@
-<<<<<<< HEAD
-const { Model, DataTypes}=require('sequelize');
-const sequelize = require('../config/connection');
-
-//inherits peroperties from the Model class.
-class Dream extends Model{};
-
-//create columns for Dream model
-Dream.init(
-    {
-        dream_id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        dream_story:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate:{
-                len: [10]
-            }
-        },
-        post_url:{
-            type: DataTypes.STRING,
-            allowNull:false,
-            validate:{
-                isURL: true,
-            }
-        },
-        upvote:{
-            type: DataTypes.INTEGER
-        },
-        downvote:{
-            type: DataTypes.INTEGER
-        }
-    },
-    {
-        sequelize,
-        freezeTableNames: true,
-        underscored: true,
-        modelName: 'dream'
-    }
-);
-
-module.exports = Dream;
-=======
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 // create our Dream model
 class Dream extends Model {}
 
 // create fields/columns for Dream model
 Dream.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      Dream_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isURL: true
-        }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'Dream'
-    }
-  );
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dream_story: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [10],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    upvote: {
+      type: DataTypes.INTEGER,
+    },
+    downvote: {
+      type: DataTypes.INTEGER,
+  },
+},
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Dream",
+  }
+);
 
-
-  module.exports = Dream;
->>>>>>> c7147959f7352d3afbda1e4c295027715626ce69
+module.exports = Dream;
