10eab024c5ca9ca5fc5a3b0fe657b7ffba59ca81
night <yseddiq@gmail.com>
2022-09-18 18:53:01 -0400

    added variables that will be used to capture data from post-dream.handlebars

--- public/javascript/postDream.js
+++ public/javascript/postDream.js
@@ -1,3 +1,8 @@
-
+async function newDreamHandler(event){
+    event.preventDefault();
+    
+    const title = document.querySelector('input[name="dream-title"]').value;
+    const dreamPost=document.querySelector('textarea[name="dream-story"]').value;
+}
 
 documnet.querySelector('#post-dream-form').addEventListener('submit', newDreamHandler);
