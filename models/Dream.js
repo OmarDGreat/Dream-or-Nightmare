ecb5424bccd24336c57a6cc56a66d66f588025a4
night <yseddiq@gmail.com>
2022-09-18 19:48:30 -0400

    fixed merge conflict

--- models/Dream.js
+++ models/Dream.js
@@ -1,50 +1,15 @@
-<<<<<<< HEAD
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
-=======
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
->>>>>>> 40b154cfa65d48efe20b59ea5cbba59d37b94e80
-
 // create our Dream model
 class Dream extends Model {}
-
 // create fields/columns for Dream model
 Dream.init(
-<<<<<<< HEAD
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      dream_story:{
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate:{
-            len: [10]
-        }
-    },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
-=======
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
->>>>>>> 40b154cfa65d48efe20b59ea5cbba59d37b94e80
     },
     title: {
       type: DataTypes.STRING,
@@ -78,10 +43,4 @@ Dream.init(
     modelName: "Dream",
   }
 );
-
-<<<<<<< HEAD
-
-  module.exports = Dream;
-=======
 module.exports = Dream;
->>>>>>> 40b154cfa65d48efe20b59ea5cbba59d37b94e80
