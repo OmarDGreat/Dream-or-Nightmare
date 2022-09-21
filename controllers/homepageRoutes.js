7aad7368b9c171628aeacefbd8b64e5191b9be90
night <yseddiq@gmail.com>
2022-09-21 02:20:30 -0400

    added handlebars code to server.js

--- controllers/homepageRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-
-router.get('/homepage',(req,res)=>{
-    res.send('enter site here')
-});
-
-module.exports = router;
