8f226f397270b407c46f1440fba2d65206f5f30a
night <yseddiq@gmail.com>
2022-09-21 19:27:47 -0400

    used a different library to create the share buttons, but it works

--- public/javascript/shareButton.js
+++ public/javascript/shareButton.js
@@ -1,3 +1,3 @@
-const  {ShareLink}  = require('../../node_modules/social-media-sharing/lib/index.js');
-let socialMediaLinks = new ShareLink(twitter);
+const  {ShareLink}  = require('social-media-sharing');
+let socialMediaLinks = new ShareLink('twitter');
 let shareLink = socialMediaLinks.get({ url })
