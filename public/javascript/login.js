aec7fb720d39c9a779e9ddf26912a7dae514dc91
OmarD <omar.daghestani@colorado.edu>
2022-09-22 18:17:56 -0600

    test 2

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -41,7 +41,7 @@ async function signupFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
